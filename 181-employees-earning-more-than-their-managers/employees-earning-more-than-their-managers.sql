49f3bff155f3900fb7871923c5f97c9db00449b9
Shriyash Jagtap <136837295+ShriyashAngadJagtap@users.noreply.github.com>
2025-07-02 23:25:47 +0530

    Time: 433 ms (22.96%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
 SELECT e2.name as Employee
 FROM employee e1
+# TO check Innner joints with in the table   wroted a  Inner join query
 INNER JOIN employee e2 ON e1.id = e2.managerID
 WHERE
 e1.salary < e2.salary
